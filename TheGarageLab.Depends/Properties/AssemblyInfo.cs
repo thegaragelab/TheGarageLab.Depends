52e209b7de97a8fae1a0ffb91fedc2c851e31958
Shane Gough <shane@thegaragelab.com>
2017-03-31 09:03:00 +0700

    Set 'beta' (odd numbered) version before I forget.

--- TheGarageLab.Depends/Properties/AssemblyInfo.cs
+++ TheGarageLab.Depends/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TheGarageLab.Depends")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Very Simple Dependency Injection")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Garage Lab")]
 [assembly: AssemblyProduct("TheGarageLab.Depends")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
